ca390cf394952dcd6f3848a4ae623e809c7fb5ea
tamafull <tamastatic@gmail.com>
2021-11-28 16:14:05 +0900

    searchでDatabaseのみ取得する

--- app.rb
+++ app.rb
@@ -11,36 +11,39 @@ module Notion
     DATABASE_URL = BASE_URL + DATABASE_API_PATH +  '/' + ENV['NOTION_DATABASE_ID']
 
     # NOTE: https://developers.notion.com/reference/post-search
-    def self.search(query)
+    def self.search(query) # TODO: タイトル以外で検索できない？
       url = BASE_URL + SEARCH_PATH
 
-      # TODO: 多分、ここで取得したIDのハイフンを取り除いたものを指定してDB取得できる
-      HTTP[
-        'Authorization': "Bearer #{ENV['NOTION_KEY']}",
-        'Content-Type': 'application/json',
-        'Notion-Version': '2021-08-16'
-      ].post(url, json: {
-        query: query,
-        sort:{
-          direction: 'ascending',
-          timestamp: 'last_edited_time'
-        }
-      })
+      response =
+        HTTP[
+          'Authorization': "Bearer #{ENV['NOTION_KEY']}",
+          'Content-Type': 'application/json',
+          'Notion-Version': '2021-08-16'
+        ].post(url, json: {
+          query: query,
+          sort:{
+            direction: 'ascending',
+            timestamp: 'last_edited_time'
+          }
+        })
+      response_json = HTTP::MimeType::JSON.decode(response)
+
+      response_json['results'].select {|result| result['object'] == 'database'} # TODO: page用も必要なのでどこかに切り出す
     end
 
     # NOTE: https://developers.notion.com/reference/retrieve-a-database
     def self.get
-      HTTP[
+      HTTP::MimeType::JSON.decode(HTTP[
         'Authorization': "Bearer #{ENV['NOTION_KEY']}",
         'Notion-Version': '2021-08-16'
-      ].get(DATABASE_URL)
+      ].get(DATABASE_URL))
     end
   end
 end
 
 response = Notion::Database.get if ARGV[0] == 'db'
-response = Notion::Database.search(ARGV[1]) if ARGV[0] == 'search' && !ARGV[1].nil?
-
-response.body.each do |r|
-  p r
+if ARGV[0] == 'search' && !ARGV[1].nil?
+  response = Notion::Database.search(ARGV[1])
 end
+
+p response
