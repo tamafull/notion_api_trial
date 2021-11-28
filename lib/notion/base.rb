ed3e8cbe31151c3e5e416b8fd7b09ff8f8cf9fd3
tamafull <tamastatic@gmail.com>
2021-11-28 22:24:50 +0900

    DatabaseからPageでも使用するものを切り出し

--- lib/notion/base.rb
+++ lib/notion/base.rb
@@ -7,6 +7,38 @@ module Notion
     BASE_URL = 'https://api.notion.com/'
     SEARCH_PATH = 'v1/search'
 
+    attr_reader :id
+    attr_reader :metadata
+
+    def initialize(id=nil, metadata={})
+      @id = id
+      @metadata = metadata
+    end
+
+    def search(query, object)
+      url = get_api_url(SEARCH_PATH)
+
+      response =
+        HTTP[
+          'Authorization': "Bearer #{ENV['NOTION_KEY']}",
+          'Content-Type': 'application/json',
+          'Notion-Version': ENV['NOTION_VERSION']
+        ].post(url, json: {
+          query: query,
+          # TODO: filterでやりたい
+          sort:{
+            direction: 'ascending',
+            timestamp: 'last_edited_time'
+          }
+        })
+      response_json = HTTP::MimeType::JSON.decode(response)
+
+      @metadata = response_json['results'].find {|result| result['object'] == object}
+      @id = convert_for_url(@metadata['id'])
+
+      self
+    end
+
     private
 
     def get_api_url(*path)
--- lib/notion/database.rb
+++ lib/notion/database.rb
@@ -4,14 +4,6 @@ module Notion
   class Database < Notion::Base
     DATABASE_API_PATH = 'v1/databases'
 
-    attr_reader :id
-    attr_reader :metadata
-
-    def initialize(id=nil, metadata={})
-      @id = id
-      @metadata = metadata
-    end
-
     def self.search(query) # TODO: タイトル以外で検索できない？
       instance = new
       instance.search(query)
@@ -19,28 +11,8 @@ module Notion
       instance
     end
 
-    def search(query)
-      url = get_api_url(SEARCH_PATH)
-
-      response =
-        HTTP[
-          'Authorization': "Bearer #{ENV['NOTION_KEY']}",
-          'Content-Type': 'application/json',
-          'Notion-Version': ENV['NOTION_VERSION']
-        ].post(url, json: {
-          query: query,
-          # filter:{
-          #   value: 'database'
-          # }, # TODO: 使い方がわからず取れない。できればselectせずに取得する段階でfilterをかけたい。
-          sort:{
-            direction: 'ascending',
-            timestamp: 'last_edited_time'
-          }
-        })
-      response_json = HTTP::MimeType::JSON.decode(response)
-
-      @metadata = response_json['results'].find {|result| result['object'] == 'database'} # TODO: page用も必要なのでどこかに切り出す
-      @id = convert_for_url(@metadata['id'])
+    def search(query, object='database')
+      super
     end
 
     # NOTE: https://developers.notion.com/reference/retrieve-a-database
